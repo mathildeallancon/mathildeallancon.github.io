10d8ca566973b741011b9f2ee6f1f0196ef08c34
Mathilde <mathilde.allancon@gmail.com>
2017-04-25 23:34:06 -0700

    Final Project Mathilde Allançon

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	
+
+	$("#link").click(function() {$(".list-item").show();
+	});
+
+	
+	$(".submit-button").click(function() {$(".list-item").hide();
+	});
+
+	
+	$(".cancel-button").click(function() {$(".list-item").hide();
+	});
+	
+	
+	$("#modal-overlay").click(function() {$(".list-item").hide();
+	});
+
+	});
+	
