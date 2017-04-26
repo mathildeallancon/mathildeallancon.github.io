008079706d5f2bca776a19e3e4cacec064e9e062
Mathilde <mathilde.allancon@gmail.com>
2017-04-25 23:12:56 -0700

    Panel Page html

---
+++ website_project/loading page/assets/js/jquery-1.9.1.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	
+
+var el = document.querySelector('.js-fade');
+
+fadeOut(el);
+fadeIn(el);
+fadeIn(el, "logo)image");
+   
+});
+
+
+ 
+
+$(".logo-image").click(function() {$(".logo-image").hide();
+	});
+	
+	$(".submit-button").click(function() {$(".list-item").hide();
+	});
+
+	
+	$(".cancel-button").click(function() {$(".list-item").hide();
+	});
+	
+	
+	$("#modal-overlay").click(function() {$(".list-item").hide();
+	});
+
+	});
